483ad980b6e23ac86e78a501ccf016b011f85ed7
leqlong <quanglong2000@gmail.com>
2023-12-18 17:58:37 -0800

    Update table_schemata.sql

--- table_schemata.sql
+++ table_schemata.sql
@@ -1,59 +1,59 @@
 -- Create the Employees table
 CREATE TABLE employees (
-    employee_number INT PRIMARY KEY,
+    emp_no INT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     birth_date DATE NOT NULL,
     hire_date DATE NOT NULL,
     gender CHAR(1) NOT NULL,
-    CONSTRAINT unique_employee_number UNIQUE (employee_number)
+    CONSTRAINT unique_emp_no UNIQUE (emp_no)
 );
 
 -- Create the Departments table
 CREATE TABLE departments (
-    department_number INT PRIMARY KEY,
+    dept_no VARCHAR(4) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    CONSTRAINT unique_department_number UNIQUE (department_number)
+    CONSTRAINT unique_dept_no UNIQUE (dept_no)
 );
 
 -- Create the Salaries table
 CREATE TABLE salaries (
     salary_id SERIAL PRIMARY KEY,
-    employee_number INT NOT NULL,
+    emp_no INT NOT NULL,
     salary INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    CONSTRAINT fk_employee_salary FOREIGN KEY (employee_number) REFERENCES employees (employee_number)
+    CONSTRAINT fk_employee_salary FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- Create the Titles table
 CREATE TABLE titles (
     title_id SERIAL PRIMARY KEY,
-    employee_number INT NOT NULL,
+    emp_no INT NOT NULL,
     title VARCHAR(50) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    CONSTRAINT fk_employee_title FOREIGN KEY (employee_number) REFERENCES employees (employee_number)
+    CONSTRAINT fk_employee_title FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- Create the Dept_emp table
 CREATE TABLE dept_emp (
     dept_emp_id SERIAL PRIMARY KEY,
-    employee_number INT NOT NULL,
-    department_number INT NOT NULL,
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    CONSTRAINT fk_employee_dept FOREIGN KEY (employee_number) REFERENCES employees (employee_number),
-    CONSTRAINT fk_department_emp FOREIGN KEY (department_number) REFERENCES departments (department_number)
+    CONSTRAINT fk_employee_dept FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    CONSTRAINT fk_department_emp FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 -- Create the Dept_manager table
 CREATE TABLE dept_manager (
     dept_manager_id SERIAL PRIMARY KEY,
-    department_number INT NOT NULL,
-    employee_number INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    CONSTRAINT fk_department_manager FOREIGN KEY (department_number) REFERENCES departments (department_number),
-    CONSTRAINT fk_employee_manager FOREIGN KEY (employee_number) REFERENCES employees (employee_number)
+    CONSTRAINT fk_department_manager FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    CONSTRAINT fk_employee_manager FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
